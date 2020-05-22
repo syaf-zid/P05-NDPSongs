d832c3e891917f045f0e65fb9920e33d211edcab
syafzid <18052270@myrp.edu.sg>
2020-05-22 21:09:43 +0800

    Completed with Song class

--- app/src/main/java/com/example/p05_ndpsongs/DBHelper.java
+++ app/src/main/java/com/example/p05_ndpsongs/DBHelper.java
@@ -1,6 +1,39 @@
 package com.example.p05_ndpsongs;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DBHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "ndpsongs.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_SONG = "song";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_TITLE = "title";
+    private static final String COLUMN_YEAR = "year";
+    private static final String COLUMN_STARS = "stars";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createSongTableSql = "CREATE TABLE " + TABLE_SONG + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_TITLE + " TEXT,"
+                + COLUMN_YEAR + " TEXT,"
+                + COLUMN_STARS + " INTEGER)";
+        db.execSQL(createSongTableSql);
+        Log.i("info", "created tables");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SONG);
+        onCreate(db);
+    }
+
+
 }
--- app/src/main/java/com/example/p05_ndpsongs/Song.java
+++ app/src/main/java/com/example/p05_ndpsongs/Song.java
@@ -1,4 +1,37 @@
 package com.example.p05_ndpsongs;
 
 public class Song {
+    private int _id;
+    private String title;
+    private String singers;
+    private int year;
+    private int stars;
+
+    public Song(int _id, String title, String singers, int year, int stars) {
+        this._id = _id;
+        this.title = title;
+        this.singers = singers;
+        this.year = year;
+        this.stars = stars;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getStars() {
+        return stars;
+    }
 }
